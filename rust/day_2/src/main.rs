19a378a3e865f175dc6c43d645ae6374f9a18aa7
vaiciits <vaicekauskis.m@gmail.com>
2023-12-05 22:25:29 +0200

    refactor: day 2 part 1

--- rust/day_2/src/main.rs
+++ rust/day_2/src/main.rs
@@ -4,7 +4,6 @@ use std::fs;
 
 fn main() {
     let contents = load_input();
-    // println!("{}", &contents);
     let sum = callibration_value_sum(&contents);
     println!("Sum: {}", sum);
 }
@@ -14,28 +13,11 @@ fn callibration_value_sum(contents: &str) -> u32 {
 
     for line in contents.lines() {
         let data = line.split(':').collect::<Vec<&str>>();
-        let id: u32 = data[0].split(' ').collect::<Vec<&str>>()[1]
-            .parse()
-            .unwrap();
-        let sets = data[1].split(';').collect::<Vec<&str>>();
-        let mut valid = true;
 
-        for set in sets {
-            let cubes = set.split(',').collect::<Vec<&str>>();
-            for cube in cubes {
-                let parts = cube.trim().split(' ').collect::<Vec<&str>>();
-                let color = parts[1];
-                let count = parts[0].parse::<u32>().unwrap();
-                let colors = colors();
-
-                if colors[color] < count {
-                    valid = false;
-                }
-            }
-        }
-
-        if valid {
-            sum += id;
+        if game_valid(data[1]) {
+            sum += data[0].split(' ').collect::<Vec<&str>>()[1]
+                .parse::<u32>()
+                .unwrap();
         }
     }
 
@@ -50,9 +32,27 @@ fn colors() -> HashMap<&'static str, u32> {
     colors
 }
 
+fn game_valid(data: &str) -> bool {
+    let sets = data.split(';').collect::<Vec<&str>>();
+
+    for set in sets {
+        let cubes = set.split(',').collect::<Vec<&str>>();
+        for cube in cubes {
+            let parts = cube.trim().split(' ').collect::<Vec<&str>>();
+            let color = parts[1];
+            let count = parts[0].parse::<u32>().unwrap();
+
+            if colors()[color] < count {
+                return false;
+            }
+        }
+    }
+
+    true
+}
+
 fn load_input() -> String {
     let args: Vec<String> = env::args().collect();
     let file_name = &args[1];
-    let contents = fs::read_to_string(file_name).expect("Something went wrong reading the file");
-    contents
+    fs::read_to_string(file_name).expect("Something went wrong reading the file")
 }
