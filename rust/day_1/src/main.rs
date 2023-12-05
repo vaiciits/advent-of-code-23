3fce9791bfe7a6cde563b70246a90ef812b59835
vaiciits <vaicekauskis.m@gmail.com>
2023-12-05 20:36:59 +0200

    refactor: day 1 part 1 with option

--- rust/day_1/src/main.rs
+++ rust/day_1/src/main.rs
@@ -1,42 +1,51 @@
 use std::env;
 use std::fs;
 
-fn callibration_value_sum(contents: &str) -> i32 {
+fn callibration_value_sum(contents: &str) -> u32 {
     let mut sum = 0;
 
     for line in contents.lines() {
-        let first = first_number(line);
-        let last: i32 = last_number(line);
-        sum += first * 10 + last;
+        let first: u32;
+        let last: u32;
+
+        match first_number(line) {
+            Some(number) => {
+                first = number;
+            }
+            None => continue,
+        }
+
+        match last_number(line) {
+            Some(number) => {
+                last = number;
+            }
+            None => continue,
+        }
+
+        sum += (first * 10) + last;
     }
 
     sum
 }
 
-fn first_number(line: &str) -> i32 {
-    let mut first = 0;
-
+fn first_number(line: &str) -> Option<u32> {
     for c in line.chars() {
         if c.is_digit(10) {
-            first = c.to_digit(10).unwrap() as i32;
-            break;
+            return Some(c.to_digit(10).unwrap() as u32);
         }
     }
 
-    first
+    None
 }
 
-fn last_number(line: &str) -> i32 {
-    let mut last = 0;
-
+fn last_number(line: &str) -> Option<u32> {
     for c in line.chars().rev() {
         if c.is_digit(10) {
-            last = c.to_digit(10).unwrap() as i32;
-            break;
+            return Some(c.to_digit(10).unwrap() as u32);
         }
     }
 
-    last
+    None
 }
 
 fn main() {
