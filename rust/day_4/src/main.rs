ce80b104812cd1065ee9b8e08ef6d112d41d6956
vaiciits <vaicekauskis.m@gmail.com>
2023-12-10 20:40:50 +0200

    feat: solve day 4 part 2

--- rust/day_4/src/main.rs
+++ rust/day_4/src/main.rs
@@ -1,17 +1,31 @@
-use std::{env, fs};
+use std::{collections::HashMap, env, fs};
 
 fn main() {
     let contents: String = load_input();
-    let sum_of_points = sum_of_points(&contents);
+    let sum_of_points: u32 = sum_of_points(&contents);
     println!("Sum of points: {}", sum_of_points);
+    let sum_of_cards: u32 = sum_of_cards(&contents);
+    println!("Sum of cards: {}", sum_of_cards);
+}
+
+fn card_numbers(line: &str) -> Vec<u32> {
+    let parts: Vec<&str> = line.split(':').collect();
+    let all_numbers: Vec<&str> = parts[1].split('|').collect();
+    all_numbers[1]
+        .trim()
+        .split_whitespace()
+        .collect::<Vec<&str>>()
+        .iter()
+        .map(|&x| x.parse::<u32>().unwrap())
+        .collect()
 }
 
 fn card_points(line: &str) -> u32 {
-    let winning_numbers = winning_numbers(line);
+    let winning_numbers: Vec<u32> = winning_numbers(line);
     // println!("winning_numbers: {:?}", winning_numbers);
-    let card_numbers = card_numbers(line);
+    let card_numbers: Vec<u32> = card_numbers(line);
     // println!("card_numbers: {:?}", card_numbers);
-    let common_numbers = common_numbers(winning_numbers, card_numbers);
+    let common_numbers: Vec<u32> = common_numbers(winning_numbers, card_numbers);
     // println!("common_numbers: {:?}", common_numbers);
     let points: u32 = if common_numbers.len() == 0 {
         0
@@ -29,22 +43,55 @@ fn common_numbers(winning: Vec<u32>, card: Vec<u32>) -> Vec<u32> {
         .collect()
 }
 
+/// Adds initial cards to cards HashMap. Each line is a card.
+fn init_cards(lines: Vec<&str>) -> HashMap<usize, u32> {
+    let mut line_index: usize = 0;
+    let mut cards: HashMap<usize, u32> = HashMap::new();
+    lines.iter().for_each(|&_x| {
+        cards.insert(line_index + 1, 1);
+        line_index += 1;
+    });
+
+    cards
+}
+
 fn load_input() -> String {
     let args: Vec<String> = env::args().collect();
     let file_name = &args[1];
     fs::read_to_string(file_name).expect("Something went wrong reading the file")
 }
 
-fn card_numbers(line: &str) -> Vec<u32> {
-    let parts: Vec<&str> = line.split(':').collect();
-    let all_numbers: Vec<&str> = parts[1].split('|').collect();
-    all_numbers[1]
-        .trim()
-        .split_whitespace()
-        .collect::<Vec<&str>>()
-        .iter()
-        .map(|&x| x.parse::<u32>().unwrap())
-        .collect()
+fn sum_of_cards(contents: &str) -> u32 {
+    let lines: Vec<&str> = contents.lines().collect();
+    let lines_length: usize = lines.len();
+    let mut line_index: usize = 0;
+    let mut cards: HashMap<usize, u32> = init_cards(lines.clone());
+
+    while line_index < lines_length {
+        let line: &str = lines[line_index];
+        // println!("{}: {}", line_index, line);
+        // let card_count: u32 = card_count(line);
+        let winning_numbers: Vec<u32> = winning_numbers(line);
+        // println!("winning_numbers: {:?}", winning_numbers);
+        let card_numbers: Vec<u32> = card_numbers(line);
+        // println!("card_numbers: {:?}", card_numbers);
+        let common_numbers: Vec<u32> = common_numbers(winning_numbers, card_numbers);
+        // println!("common_numbers: {:?}", common_numbers);
+        let mut card_index: usize = line_index + 2;
+        let card_count: usize = common_numbers.len();
+
+        while card_index < line_index + 2 + card_count {
+            let card_value: u32 = cards[&(line_index + 1)];
+            // println!("card_index: {}", card_index);
+            cards.insert(card_index, cards[&card_index] + card_value);
+            card_index += 1;
+        }
+
+        // println!("cards: {:?}", cards);
+        line_index += 1;
+    }
+
+    total_cards(cards)
 }
 
 fn sum_of_points(contents: &str) -> u32 {
@@ -64,6 +111,10 @@ fn sum_of_points(contents: &str) -> u32 {
     }
 
     sum
+}
+
+fn total_cards(cards: HashMap<usize, u32>) -> u32 {
+    cards.values().sum()
 }
 
 fn winning_numbers(line: &str) -> Vec<u32> {
